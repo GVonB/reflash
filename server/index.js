ae3eff037773253854268e633b2d2ae53e75c59d
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-01-26 17:38:16 -0500

    Use Vite to setup client folder with react

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     }
